4494ef72a7093c933c939e82d071f1a7b8152bd6
fuxi <806941929@qq.com>
2025-04-02 15:02:07 +0800

    增加调试日志

--- android/src/newarch/java/com/hector/nativewechat/NativeWechatPackage.java
+++ android/src/newarch/java/com/hector/nativewechat/NativeWechatPackage.java
@@ -7,17 +7,16 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.util.Log;
 
 public class NativeWechatPackage extends BaseReactPackage {
 
   @Nullable
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+    Log.e("NativeWechatPackage", name);
+    Log.e("NativeWechatPackage", NativeWechatModule.NAME);
     if (name.equals(NativeWechatModule.NAME)) {
       return new NativeWechatModule(reactContext);
     } else {
